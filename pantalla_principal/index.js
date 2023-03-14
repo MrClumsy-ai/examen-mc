71aafb1220408071a0792d60d31209b3d4f43dc5
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2023-03-13 23:02:18 -0500

    modified:   pantalla_principal/index.js

--- pantalla_principal/index.js
+++ pantalla_principal/index.js
@@ -1,51 +1,151 @@
+const MATRICULA = 2077725;
+const NOMBRE = "Eduardo Menchaca"
+const LLAVE_SECRETA = "hi"
 $(document).ready(function () {
-  // agregar publicaciones recientes
-  // $("#publicaciones").prepend(`
-  // `);
+  
+  agregarPublicaciones()
 });
 
-let id = 0; // quitar esto luego
-// nueva publicacion
-$("#publicar-btn").click(function (event) {
-  let numLikes = 0;
-  let numComenarios = 0;
-  id++; // quitar esto luego
+function agregarPublicaciones() {
+  $.ajax({
+    url: "https://ex2r.fime.uanl.mx/api/Publicaciones/all/"+MATRICULA,
+    type: "GET",
+    datatype: "json",
+    crossdomain: true
+  }).done(function (result) {
+    result.forEach(element => { 
+      
+      $("#publicaciones").append(`
+      
+        <div id="${element.idPublicacion}" class="content">
+          <h5 class="${element.idUsuario}">${element.nombre}
+            <span>${element.idUsuario}</span>
+          </h5>
+          <h6>${element.fechaCreacion}</h6>
+          <p class="contenido-publicacion">${element.contenido}</p>
+          <div class="acciones-div">
+            <button class="btn btn-dark like-btn">${element.cantidadLikes} likes</button>
+            <button class="btn btn-dark comentar-btn">comenta algo</button>
+            <button class="btn btn-dark mostrar-comentarios">Mostrar todos los comentarios(${element.cantidadComentarios})</button>
+          </div>
+        </div>
+      
+      `)
+    });
+  }).fail(function(error) {
+    console.log("error")
+  })
+}
 
-  if (!($("#publicacion-field").val().trim() == "")) {
-    let name = "Joe biden" // quitar esto luego
+let idPublicacion = 0;
+// nueva publicacion
+// unauthorized
+// llave secreta ???????????????????????????????????????/
+$("#publicar-btn").click(function (event) {
+
+  let contenido = $("#publicacion-field").val().trim()
+
+  if (!contenido == "") {
+
+    console.log(idPublicacion, MATRICULA, contenido)
+
+    $.ajax({
+      url: "https://ex2r.fime.uanl.mx/api/Publicaciones",
+      method: "POST",
+      data: JSON.stringify({
+        "idPublicacion": idPublicacion,
+        "idUsuario": MATRICULA,
+        "llave_Secreta": MATRICULA.toString,
+        "contenido": contenido
+      }),
+      contentType: "application/json"
+      
+    }).done(function (result) {
+      // $("#publicaciones").prepend(
+      //   `<div id=${idPublicacion} class="content">
+      //     <h5 id="${MATRICULA}">
+      //       ${NOMBRE}
+      //       <span>${MATRICULA}</span>
+      //     </h5>
+      //     <button class="borrar">borrar</button>
+      //     <button class="editar">editar</button>
+      //     <p class="contenido-publicacion">
+      //       ${contenido}
+      //     </p>
+      //     <div class="acciones-div">
+      //       <button class="btn btn-dark like-btn">0 Likes</button>
+      //       <button class="btn btn-dark comentar-btn">Comenta algo</button>
+      //       <button class="btn btn-info mostrar-comentarios">Mostrar todos los comentarios (0)</button>
+      //     </div>
+      //   </div>`
+      // );
+    }).fail(function (a, b, c) {
+      console.log(b, c)
+    })
+    // borrar esto luego
     $("#publicaciones").prepend(
-      `<div id="${id}" class="content">
-        <h5>
-          ${name}
+      `<div id=${idPublicacion} class="content">
+        <h5 id="${MATRICULA}">
+          ${NOMBRE}
+          <span>${MATRICULA}</span>
         </h5>
         <button class="borrar">borrar</button>
         <button class="editar">editar</button>
         <p class="contenido-publicacion">
-          ${$("#publicacion-field").val()}
+          ${contenido}
         </p>
         <div class="acciones-div">
-          <button class="btn btn-dark like-btn">${numLikes} Likes</button>
+          <button class="btn btn-dark like-btn">0 Likes</button>
           <button class="btn btn-dark comentar-btn">Comenta algo</button>
-          <button class="btn btn-info mostrar-comentarios">Mostrar todos los comentarios (${numComenarios})</button>
+          <button class="btn btn-info mostrar-comentarios">Mostrar todos los comentarios (0)</button>
         </div>
       </div>`
     );
+    
     $("#publicacion-field").val("");
+    idPublicacion++
   }
   event.preventDefault()
 });
 
 // borrar publicacion
+// unauthorized
 $("#publicaciones").on("click", ".borrar", function () {
-  console.log("publicacion #" + $(this).closest("div").attr("id") + " borrada");
-  $(this).closest("div").remove();
+  let idPublicacion = $(this).closest("div").attr("id")
+  let contenido = $(this).siblings("p").text().trim()
+  $.ajax({
+
+    url: "https://ex2r.fime.uanl.mx/api/Publicaciones/"+idPublicacion,
+    method: "DELETE",
+    data: JSON.stringify({
+      "idPublicacion": idPublicacion,
+      "idUsuario": MATRICULA,
+      "llave_Secreta": LLAVE_SECRETA,
+      "contenido": contenido
+    }),
+    contentType: "application/json"
+    
+  }).done(function (result) {
+
+    console.log("yay")
+    $(this).closest("div").remove();
+
+  }).fail(function (a, b, c) {
+
+    console.log(b, c)
+  })
+
 });
 
 let editando = false;
 // editar publicacion
+// unauthorized
 $("#publicaciones").on("click", ".editar", function () {
   // hace para que solo salga 1 caja de editar
   if (!editando) {
+    let idPublicacion = $(this).closest("div").attr("id")
+    console.log(idPublicacion)
+    
     editando = true;
     console.log(
       "editando publicacion #" + $(this).closest("div").attr("id") + "..."
@@ -85,9 +185,28 @@ $("#publicaciones").on("click", ".editar", function () {
       let field = $(this).parent().find(".editar-field").val().trim();
       if (!(field === "")) {
         $(this).parent().siblings(".contenido-publicacion").text(field);
+        let idPublicacion = $(this).parent().parent().attr("id")
+        console.log($(this).parent().parent().attr("id"))
+        console.log("dando:", idPublicacion, MATRICULA, LLAVE_SECRETA, field)
+        $.ajax({
+          url: "https://ex2r.fime.uanl.mx/api/Publicaciones/"+idPublicacion,
+          method: "PUT",
+          data: JSON.stringify({
+
+            "idPublicacion": idPublicacion,
+            "idUsuario": MATRICULA,
+            "llave_Secreta": LLAVE_SECRETA,
+            "contenido": field
+          }),
+          contentType: "application/json"
+        }).done(function (result) {
+          console.log("yay")
+          console.log("done!");
+        }).fail(function (a, b, c) {
+          console.log(b, c)
+        })
         $("#editar").remove();
         editando = false;
-        console.log("done!");
       } else {
         console.log("no se puede re-publicar un string vacio");
       }
@@ -96,23 +215,40 @@ $("#publicaciones").on("click", ".editar", function () {
 });
 
 // like
-let liked = false;
-let likes = 0;
+// unauthorized
 $("#publicaciones").on("click", ".like-btn", function () {
-  // dar like
-  if (!liked) {
-    liked = true;
-    likes++;
-    $(this).text(`${likes} Likes`);
+
+  let idPublicacion = $(this).parent().parent().attr("id")
+  let idUsuario = $(this).parent().parent().children("h5").attr("class")
+  $.ajax({
+    url: "https://ex2r.fime.uanl.mx/api/Likes",
+    method: "POST",
+    data: JSON.stringify({
+      "idPUblicacion": idPublicacion,
+      "idUsuario": idUsuario,
+      "llave_Secreta": LLAVE_SECRETA
+    }),
+    contentType: "application/json"
+  }).done(function (result) {
+
     $(this).css("background-color", "var(--like-color)");
-  }
-  // quitar like
-  else {
-    liked = false;
-    likes--;
-    $(this).text(`${likes} Likes`);
-    $(this).css("background-color", "var(--sin-like-color)");
-  }
+    console.log("yay")
+  }).fail(function (a, b, c) {
+    console.log(b, c)
+  })
+  // if (!liked) {
+  //   liked = true;
+  //   likes++;
+  //   $(this).text(`${likes} Likes`);
+  //   $(this).css("background-color", "var(--like-color)");
+  // }
+  // // quitar like
+  // else {
+  //   liked = false;
+  //   likes--;
+  //   $(this).text(`${likes} Likes`);
+  //   $(this).css("background-color", "var(--sin-like-color)");
+  // }
 });
 
 // dar comentario
@@ -122,6 +258,7 @@ $("#publicaciones").on("click", ".comentar-btn", function () {
   if (!comentando) {
     comentando = true;
     console.log("comentando...");
+    console.log(this)
     $(this).siblings(".mostrar-comentarios").after(`
     
       <div id="comentario-div" class="mt-3">
